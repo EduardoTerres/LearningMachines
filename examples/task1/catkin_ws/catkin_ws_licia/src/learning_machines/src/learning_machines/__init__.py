6b5ae4cd6b222e05f1acea06daaf2b0fb7843201
EduardoTerres <169451532+EduardoTerres@users.noreply.github.com>
2026-01-13 19:26:49 +0100

    delete old files

--- examples/task1/catkin_ws/catkin_ws_licia/src/learning_machines/src/learning_machines/__init__.py
+++
@@ -1,6 +0,0 @@
-from robobo_interface import IRobobo
-from .mycontroller import find_object_and_turnR
-from .plots import plot_all_sensors
-
-
-__all__ = ("find_object_and_turnR", "plot_all_sensors")
